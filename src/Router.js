04aaf9339d11e21cda796c4c0523c8fc0cb6a900
Navdeep Singh <navdeep.er@gmail.com>
2019-10-01 16:42:56 +0530

    Add: React Context API

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// Import context provider
+import { MyProvider } from './Context';
 import App from './App';
 import NotFound from './NotFound';
 
 function Router() {
   return (
-    <BrowserRouter>
+    <MyProvider>
+      <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={App} />
-            <Route exact path='/home' component={App} />
-            <Route path='*' exact={true} component={NotFound} />
+          <Route exact path='/' component={App} />
+          <Route exact path='/home' component={App} />
+          <Route path='*' exact={true} component={NotFound} />
         </Switch>
-    </BrowserRouter>
+      </BrowserRouter>
+    </MyProvider>
   );
 }
 
